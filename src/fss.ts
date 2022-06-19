9f8accefaa26a188b254575f4199a4f7ceecb217
Tyler Mueller <muelltyl@gmail.com>
2022-06-19 18:44:29 -0500

    adds types

--- src/fss.ts
+++ src/fss.ts
@@ -1,11 +1,17 @@
 import * as util from './util.ts';
 
-export interface fileSystemStructureBlock {
+export interface fssBit {
     property: string,
     length: number
 }
 
-export function parseUint8ArrayFromFssBlock(a: Uint8Array, fss: Array<fileSystemStructureBlock>): Record<string,Uint8Array> {
+export type fssBlock = Array<fssBit>;
+
+export type dataBit = Record<string,Uint8Array>;
+
+export type dataBlock = Array<dataBit>;
+
+export function parseUint8ArrayFromFssBlock(a: Uint8Array, fss: fssBlock): Record<string,Uint8Array> {
     if(a.length !== fss.reduce((a,v) => a += v.length, 0)) throw new RangeError;
     const sub = util.subsecUint8Array(a, fss.map(v => v.length));
     return Object.fromEntries(fss.map((v,i) => [v.property, sub[i]]));
