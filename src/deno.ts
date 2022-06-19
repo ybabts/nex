d822f40416df83b5aa56cca34a1a69e1e3ab5f87
Tyler Mueller <muelltyl@gmail.com>
2022-06-19 12:21:45 -0500

    adds ensurePermission
    idk how to test Deno.permissions.request by design requires user input

---
+++ src/deno.ts
@@ -0,0 +1,8 @@
+
+export async function ensurePermission(p: Deno.PermissionDescriptor, state_override?: string) {
+    const perm = await Deno.permissions.query(p);
+    if((state_override || perm.state) === 'prompt') await Deno.permissions.request(p);
+    if((state_override || perm.state) === 'denied') throw new Error(`ensure Deno permission denied: ${p}`)
+    if((state_override || perm.state) === 'granted') return true;
+    throw new Error('unknown error occurred');
+}
---
+++ tests/deno.test.ts
@@ -0,0 +1,30 @@
+import { assertEquals, fail } from "https://deno.land/std@0.142.0/testing/asserts.ts";
+import * as deno from '../src/deno.ts';
+
+Deno.test({
+    name: 'deno.ensurePermssions.permission.denied',
+    fn: async () => {
+        if(await deno.ensurePermission({name: 'read'}, 'denied').catch((e) => {
+            assertEquals(e instanceof Error, true);
+        })) fail();
+    }
+});
+
+Deno.test({
+    name: 'deno.ensurePermssions.permission.granted',
+    fn: async () => {
+        await deno.ensurePermission({name: 'read'}, 'granted').catch((e) => {
+            fail(e)
+        }).then(v => assertEquals(v, true));
+    }
+});
+
+Deno.test({
+    name: 'deno.ensurePermssions.permission',
+    fn: async () => {
+        await deno.ensurePermission({name: 'read'}, 'granted').catch((e) => {
+            fail(e)
+        }).then(v => assertEquals(v, true));
+    }
+});
+
