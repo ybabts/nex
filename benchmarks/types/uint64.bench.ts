bf13189e9464cc35f099a58303f62127051bcc8b
Tyler Mueller <muelltyl@gmail.com>
2022-06-25 21:20:27 -0500

    adds uint64 benchmarks

--- benchmarks/types/uint64.bench.ts
+++ benchmarks/types/uint64.bench.ts
@@ -1,9 +1,10 @@
-import Uint32 from '../../src/types/uint32.ts';
+import Uint64 from '../../src/types/uint64.ts';
 
 const random = {
     int: (Math.random() * 1000000) | 0,
-    bigint: BigInt((Math.random() * 10000000) | 0),
-    Uint8Array: new Uint8Array([(Math.random() * 255) | 0, (Math.random() * 255) | 0, (Math.random() * 255) | 0, (Math.random() * 255) | 0])
+    bigint: BigInt((Math.random() * 1000000) | 0),
+    Uint8Array: new Uint8Array([(Math.random() * 255) | 0, (Math.random() * 255) | 0, (Math.random() * 255) | 0, (Math.random() * 255) | 0,
+        (Math.random() * 255) | 0, (Math.random() * 255) | 0, (Math.random() * 255) | 0, (Math.random() * 255) | 0])
 }
 
 const typedArrays = {
@@ -17,60 +18,38 @@ const dataViews = {
 }
 
 Deno.bench({
-    name: 'Uint32.toUint8Array()',
+    name: 'Uint64.toUint8Array()',
     fn() {
-        Uint32.toUint8Array(random.int)
+        Uint64.toUint8Array(random.bigint)
     }
 });
 
 Deno.bench({
-    name: 'Uint32 -> Uint8Array /w DataView',
+    name: 'Uint64 -> Uint8Array /w DataView',
     fn() {
-        dataViews.Uint8Array.setUint32(0, random.int); 
-        typedArrays.Uint8Array.slice(0, 4);
+        dataViews.Uint8Array.setBigUint64(0, random.bigint); 
+        typedArrays.Uint8Array.slice(0, 8);
     }
 });
 
 Deno.bench({
-    name: 'Uint32 -> Uint8Array /w Bit Shifting',
+    name: 'Uint64.fromUint8Array()',
     fn() {
-        new Uint8Array([
-            (random.int & 0xff000000) >> 24,
-            (random.int & 0x00ff0000) >> 16,
-            (random.int & 0x0000ff00) >> 8,
-            (random.int & 0x000000ff)
-        ]);
+        Uint64.fromUint8Array(random.Uint8Array)
     }
 });
 
 Deno.bench({
-    name: 'Uint32.fromUint8Array()',
-    fn() {
-        Uint32.fromUint8Array(random.Uint8Array)
-    }
-});
-
-Deno.bench({
-    name: 'Uint8Array -> Uint32 /w DataView',
+    name: 'Uint8Array -> Uint64 /w DataView',
     fn() {
         typedArrays.Uint8Array.set(random.Uint8Array);
-        dataViews.Uint8Array.getUint32(0);
+        dataViews.Uint8Array.getBigUint64(0);
     }
 });
 
 Deno.bench({
-    name: 'Uint8Array -> Uint32 /w Uint32Array',
+    name: 'Uint8Array -> Uint64 /w Uint64Array',
     fn() {
-        new Uint32Array(random.Uint8Array.buffer, random.Uint8Array.byteOffset, random.Uint8Array.byteLength / 4)[0];
-    }
-});
-
-Deno.bench({
-    name: 'Uint8Array -> Uint32 /w Bit Shifting',
-    fn() {
-        ((random.Uint8Array[random.Uint8Array.length - 1]) | 
-            (random.Uint8Array[random.Uint8Array.length - 2] << 8) | 
-            (random.Uint8Array[random.Uint8Array.length - 3] << 16) | 
-            (random.Uint8Array[random.Uint8Array.length - 4] << 24)) >>> 0;
+        new BigUint64Array(random.Uint8Array.buffer, random.Uint8Array.byteOffset, random.Uint8Array.byteLength / 8)[0]
     }
 });
